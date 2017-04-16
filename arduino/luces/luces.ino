217a6596d5e85c88a3ef84e73af6636fd53a612c
Ernesto Valero <evalero@paradigmadigital.com>
2017-04-16 20:11:16 +0200

    Modifed to get a more gradient change of light phases

--- arduino/luces/luces.ino
+++ arduino/luces/luces.ino
@@ -1,21 +1,27 @@
 int outMin = 8;
-int outMax=13;
+int outMax = 13;
 
 void setup() {
-  for (int i=outMin; i<=outMax; i++){
-    pinMode(i,OUTPUT);
+  //init lights located from pin 8 to 13
+  for (int i = outMin; i <= outMax; i++) {
+    pinMode(i, OUTPUT);
   }
+  //if analog input pin 0 is unconnected, is a good seed
+  // seen https: //www.arduino.cc/en/Reference/Random
   randomSeed(analogRead(0));
   Serial.begin(9600);
+  for (int i = outMin; i <= outMax; i++) {
+    digitalWrite(i, random(0, 2));
+  }
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (int i=outMin; i<=outMax; i++){
-    digitalWrite(i,random(0,2));
-  }
-  delay(random(3000,6000));
-  
+
+  digitalWrite (random(8,14),random(0,2));
+  delay(random(5000, 20000));
+
 
 }
